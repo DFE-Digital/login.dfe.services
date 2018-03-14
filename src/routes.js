b2214b0f19d4d92c4d77034b25a75776509a0555
Simon Bull <simon.bull.84@googlemail.com>
2018-03-14 11:35:58 +0000

    Change state mismatch check to regex to catch multiple variants of error

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const routes = (app, csrf) => {
       let redirectUrl = '/';
 
       if (err) {
-        if (err.message === 'state mismatch') {
+        if (err.message.match(/state\smismatch/)) {
           req.session = null;
           return res.redirect('/');
         }
