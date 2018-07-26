00e85dc4c382365530cb947ba8cc970079ebb918
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-07-26 09:51:59 +0100

    NSA-1645 organisations ui

--- src/app/organisations/index.js
+++ src/app/organisations/index.js
@@ -5,14 +5,13 @@ const { isLoggedIn } = require('../../infrastructure/utils');
 const logger = require('../../infrastructure/logger');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 
-const org = require('./organisations')
+const org = require('./organisations');
 
 const router = express.Router({ mergeParams: true });
 
-const signout = () => {
-  logger.info('Mounting signOut route');
+const organisations = () => {
+  logger.info('Mounting organisations route');
   router.get('/', isLoggedIn, asyncWrapper(org));
   return router;
 };
-
-module.exports = signout;
+module.exports = organisations;
--- src/app/organisations/organisations.js
+++ src/app/organisations/organisations.js
@@ -1,13 +1,64 @@
 'use strict';
 
+const { getOrganisationAndServiceForUser } = require('./../../infrastructure/organisations');
+const { getOidcClients } = require('./../../infrastructure/hotConfig');
 const Account = require('./../../infrastructure/account');
-const getAndMapOrganisationsAndServices = require('./../home/home');
+const flatten = require('lodash/flatten');
+const uniq = require('lodash/uniq');
+
+const getApproversDetails = async (organisations) => {
+  const allApproverIds = flatten(organisations.map((org) => org.approvers));
+  const distinctApproverIds = uniq(allApproverIds);
+  if (distinctApproverIds.length === 0) {
+    return [];
+  }
+  return Account.getUsersById(distinctApproverIds);
+};
+
+const mapRole = (role) => {
+  let id = role.id;
+  if (id === 0) {
+    id = 'end-user';
+  }
+  if (id === 10000) {
+    id = 'approver';
+  }
+  return {
+    id,
+    name: role.name,
+  };
+};
+const getAndMapOrganisationsAndServices = async (account, correlationId) => {
+  const organisations = await getOrganisationAndServiceForUser(account.id, correlationId);
+  const allApprovers = await getApproversDetails(organisations, correlationId);
+  const oidcClients = await getOidcClients(correlationId);
+
+  return organisations.map((organisation) => {
+    const approvers = organisation.approvers.map((approverId) => {
+      return allApprovers.find(x => x.id.toLowerCase() === approverId.toLowerCase());
+    }).filter(x => x);
+    const services = organisation.services ? organisation.services.map((service) => {
+      const oidcClient = oidcClients.find(c => c.params && c.params.serviceId && c.params.serviceId.toLowerCase() === service.id.toLowerCase());
+      const serviceUrl = oidcClient ? (oidcClient.service_home || oidcClient.redirect_uris[0] ): '#';
+      return Object.assign({ serviceUrl }, service);
+    }) : [];
+    return {
+      id: organisation.organisation.id,
+      name: organisation.organisation.name,
+      urn: organisation.organisation.urn,
+      uid: organisation.organisation.uid,
+      role: mapRole(organisation.role),
+      approvers,
+      services,
+    };
+  })
+};
 
 const organisations = async (req, res) => {
   const account = Account.fromContext(req.user);
   const organisations = await getAndMapOrganisationsAndServices(account, req.id);
 
-  return res.render('organisations/views/organisations',{
+  return res.render('organisations/views/organisations', {
     title: 'Organisations',
     user: account,
     organisations,
