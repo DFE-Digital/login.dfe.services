7acfd2ba57e9ab4f3c6237c4ca61d2a2c0871998
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-11-09 15:15:34 +0000

    Fix access api bug

--- src/infrastructure/access/api.js
+++ src/infrastructure/access/api.js
@@ -10,52 +10,56 @@ const rp = require('request-promise').defaults({
 });
 const jwtStrategy = require('login.dfe.jwt-strategies');
 
-const callApi = async (method, path, correlationId, body) => {
+const callApi = async (method, endpoint, correlationId, body) => {
   const token = await jwtStrategy(config.access.service).getBearerToken();
 
-  const hasSeperator = (config.access.service.url.endsWith('/') && !path.startsWith('/'))
-    || (!config.access.service.url.endsWith('/') && path.startsWith('/'));
-  const basePathSeperator = hasSeperator ? '' : '/';
-  const opts = {
-    method,
-    uri: `${config.access.service.url}${basePathSeperator}${path}`,
-    headers: {
-      authorization: `bearer ${token}`,
-      'x-correlation-id':
-      correlationId,
-    },
-    json: true,
-  };
-  if (body && (method === 'POST' || method !== 'PUT' || method !== 'PATCH')) {
-    opts.body = body;
+  try {
+    return await rp({
+      method,
+      uri: `${config.access.service.url}/${endpoint}`,
+      headers: {
+        authorization: `bearer ${token}`,
+        'x-correlation-id': correlationId,
+      },
+      body: body,
+      json: true,
+    });
+  } catch (e) {
+    const status = e.statusCode ? e.statusCode : 500;
+    if (status === 401 || status === 404) {
+      return undefined;
+    }
+    if (status === 409) {
+      return false;
+    }
+    throw e;
   }
-  return rp(opts);
 };
 
 
 const getServicesForUser = async (id, correlationId) => {
-  return callApi('GET', `/users/${id}/services`, correlationId);
+  return callApi('GET', `/users/${id}/services`, undefined, correlationId);
 };
 
 const getServicesForInvitation = async (iid, correlationId) => {
-  return callApi('GET', `/invitations/${iid}/services`, correlationId)
+  return callApi('GET', `/invitations/${iid}/services`, undefined, correlationId)
 };
 
 
 const getSingleUserService = async (id, sid, oid, correlationId) => {
-  return callApi('GET', `/users/${id}/services/${sid}/organisations/${oid}`, correlationId);
+  return callApi('GET', `/users/${id}/services/${sid}/organisations/${oid}`, undefined, correlationId);
 };
 
 const getSingleInvitationService = async (iid, sid, oid, correlationId) => {
-  return callApi('GET', `invitations/${iid}/services/${sid}/organisations/${oid}`, correlationId);
+  return callApi('GET', `invitations/${iid}/services/${sid}/organisations/${oid}`, undefined, correlationId);
 };
 
 const listRolesOfService = async (sid, correlationId) => {
-  return callApi('GET', `services/${sid}/roles`, correlationId);
+  return callApi('GET', `services/${sid}/roles`, undefined, correlationId);
 };
 
 const removeServiceFromUser = async (uid, sid, oid, correlationId) => {
-  return callApi('DELETE', `users/${uid}/services/${sid}/organisations/${oid}`, correlationId);
+  return callApi('DELETE', `users/${uid}/services/${sid}/organisations/${oid}`, undefined, correlationId);
 };
 
 module.exports = {
