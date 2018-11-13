11e8025b47e6cdd8216d1d5b5e850ae30aa8c920
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-11-13 15:48:13 +0000

    Check if user roles selected is an array

--- src/app/users/confirmEditService.js
+++ src/app/users/confirmEditService.js
@@ -3,14 +3,28 @@ const logger = require('./../../infrastructure/logger');
 const { getUserDetails, getSingleServiceForUser } = require('./utils');
 const { listRolesOfService, updateUserService, updateInvitationService } = require ('./../../infrastructure/access');
 
+const getSelectedRoles = async (req) => {
+  let selectedRoleIds = req.session.service.roles;
+  const allRolesOfService = await listRolesOfService(req.params.sid, req.id);
+  let roleDetails;
+
+  if (selectedRoleIds && !Array.isArray(selectedRoleIds)) {
+    selectedRoleIds = [selectedRoleIds];
+  }
+  if (selectedRoleIds) {
+    roleDetails = allRolesOfService.filter(x => selectedRoleIds.find(y=> y.toLowerCase() === x.id.toLowerCase()));
+  } else  {
+    roleDetails = [];
+  }
+  return roleDetails;
+};
+
 const get = async (req, res) => {
   const user = await getUserDetails(req);
   const userService = await getSingleServiceForUser(req.params.uid, req.params.orgId, req.params.sid, req.id);
   const organisationId = req.params.orgId;
   const organisationDetails = req.userOrganisations.filter(x => x.organisation.id === organisationId);
-  const selectedRoleIds = req.session.service.roles;
-  const allRolesOfService = await listRolesOfService(req.params.sid, req.id);
-  const roleDetails = allRolesOfService.filter(x => selectedRoleIds.find(y=> y.toLowerCase() === x.id.toLowerCase()));
+  const roleDetails = await getSelectedRoles(req);
   return res.render('users/views/confirmEditService', {
     csrfToken: req.csrfToken(),
     organisation: organisationDetails,
