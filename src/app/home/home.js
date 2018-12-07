78d864ad38bc3ea611a2e0f1bf7500a8ac5c3375
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-12-07 11:21:15 +0000

    NSA-1910- internal services redesign

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,28 +1,10 @@
 'use strict';
 
-const { getServicesForUser } = require('./../../infrastructure/access');
-const { getApplication } = require('./../../infrastructure/applications');
 const { getAllServices } = require('./../../infrastructure/applications');
 const Account = require('./../../infrastructure/account');
 const uniqBy = require('lodash/uniqBy');
 const sortBy = require('lodash/sortBy');
 
-
-const getAndMapServices = async (account, correlationId) => {
-  const serviceAccess = (await getServicesForUser(account.id, correlationId)) || [];
-  const services = uniqBy(serviceAccess.map((sa) => ({
-    id: sa.serviceId,
-    name: '',
-    serviceUrl: '',
-  })), 'id');
-  for (let i = 0; i < services.length; i++) {
-    const service = services[i];
-    const application = await getApplication(service.id);
-    service.name = application.name;
-    service.serviceUrl = (application.relyingParty ? (application.relyingParty.service_home || application.relyingParty.redirect_uris[0]) : undefined) || '#';
-  }
-  return sortBy(services, 'name');
-};
 
 const getAndMapExternalServices = async (correlationId) => {
   const allServices = await getAllServices(correlationId) || [];
@@ -38,24 +20,12 @@ const getAndMapExternalServices = async (correlationId) => {
 };
 
 const home = async (req, res) => {
-  if (req.isAuthenticated()) {
-    const account = Account.fromContext(req.user);
-    const services = await getAndMapServices(account, req.id);
-
-    return res.render('home/views/home', {
-      title: 'Access DfE services',
-      user: account,
-      services,
-      currentPage: 'services'
-    });
-  } else {
-    const services = await getAndMapExternalServices(req.id);
-    return res.render('home/views/externalServices', {
-      title: 'Access DfE services',
-      services,
-      loggedOut: true,
-    });
-  }
+  const services = await getAndMapExternalServices(req.id);
+  return res.render('home/views/externalServices', {
+    title: 'Access DfE services',
+    services,
+    loggedOut: true,
+  });
 };
 
 module.exports = home;
