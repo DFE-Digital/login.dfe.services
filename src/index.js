fa3e6919292718f2f38bbc0ff938673b96dc621e
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-04-01 15:06:23 +0100

    docker setup

--- src/index.js
+++ src/index.js
@@ -150,7 +150,9 @@ const init = async () => {
       logger.info(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port} with config:\n${JSON.stringify(config)}`);
     });
   } else if (config.hostingEnvironment.env === 'docker') {
-    logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`)
+    app.listen(config.hostingEnvironment.port, () => {
+      logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`)
+    });
   } else {
     app.listen(process.env.PORT, () => {
       logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
