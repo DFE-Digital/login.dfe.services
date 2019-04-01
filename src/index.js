e41f84339979af1e3c5fa8943db3aa60db6cdcf7
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-04-01 14:13:51 +0100

    fix typo

--- src/index.js
+++ src/index.js
@@ -150,7 +150,7 @@ const init = async () => {
       logger.info(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port} with config:\n${JSON.stringify(config)}`);
     });
   } else if (config.hostingEnvironment.env === 'docker') {
-    logger.info(`Server listening on http://${config.hostingEnvironment.port}:${config.hostingEnvironment.port}`)
+    logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`)
   } else {
     app.listen(process.env.PORT, () => {
       logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
