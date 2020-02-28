b3a6551cc6977fb8edd9bbad2ee2a385bc9f4161
DrogoNevets <steve.colemanwilliams@gmail.com>
2020-02-28 14:17:37 +0000

    fixed double new

--- src/keepAliveAgent.js
+++ src/keepAliveAgent.js
@@ -1,6 +1,6 @@
 const KeepAliveAgent = require('agentkeepalive');
 
-module.exports = new new KeepAliveAgent({
+module.exports = new KeepAliveAgent({
     maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
     maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
     timeout: config.hostingEnvironment.agentKeepAlive.timeout,
