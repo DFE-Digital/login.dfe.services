eee8c7f77820729e238138dd6a20122973876cc8
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-04-09 14:57:11 +0100

    Remove secrets from DB scripts

--- database_scripts/003_Tool_service_script_prod.sql
+++ database_scripts/003_Tool_service_script_prod.sql
@@ -2,7 +2,7 @@ BEGIN TRAN toolServiceProd
 DECLARE @serviceId UNIQUEIDENTIFIER;
 SET @serviceId = newid();
 	INSERT INTO service (id, name, description, clientId, clientSecret, apiSecret, tokenEndpointAuthMethod, serviceHome, postResetUrl, isExternalService, isMigrated, parentId, isChildService)
-	VALUES (@serviceId, 'das-tool-service', 'das-tool-service', 'dastools', 'skoaling-lacunal-reach-tithe', 'oslo-spuriously-justifier-loused', 'client_secret_post', 'https://dfe-apprenticeships.eu.auth0.com', null, 1, 0, null, 0);
+	VALUES (@serviceId, 'das-tool-service', 'das-tool-service', 'dastools', '????', '????', 'client_secret_post', 'https://dfe-apprenticeships.eu.auth0.com', null, 1, 0, null, 0);
 
 	INSERT INTO serviceRedirectUris (serviceId, redirectUrl)
 	VALUES (@serviceId, 'https://dfe-apprenticeships.eu.auth0.com/login/callback');
