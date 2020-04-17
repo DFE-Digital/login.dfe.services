8efec06d13adce824a1702238c8e94f95787b236
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2020-04-17 16:17:58 +0100

    Revert "this should solve the issue - needs testing in a proper env though"

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ const init = async () => {
   app.use(expressLayouts);
   app.set('layout', 'layouts/layout');
   app.use(session({
-    resave: false,
+    resave: true,
     saveUninitialized: true,
     secret: config.hostingEnvironment.sessionSecret,
     cookie: {
