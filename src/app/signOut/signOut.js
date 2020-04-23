64201dddd811fa0f4bbb1d580b0a58482f967a5c
Steve Coleman-Williams <steve@archronos.com>
2020-04-23 09:14:08 +0100

    go to signout complete

--- src/app/signOut/signOut.js
+++ src/app/signOut/signOut.js
@@ -19,8 +19,8 @@ const signUserOut = (req, res) => {
     const issuer = passport._strategies.oidc._issuer;
     let returnUrl;
     if (req.query.redirected === 'true') {
-      // returnUrl = `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/signout/complete`;
-      res.redirect(`${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/`);
+      returnUrl = `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/signout/complete`;
+      // res.redirect(`${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/`);
     } else {
       returnUrl = `${config.hostingEnvironment.profileUrl}/signout?redirected=true`;
     }
