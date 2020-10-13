d737743c4961b0d7a5b48ed0d12eb6ffd1e936cd
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-10-13 11:05:43 +0100

    Updates to pass asset version to error handler

--- src/index.js
+++ src/index.js
@@ -129,6 +129,7 @@ const init = async () => {
     {
       help: config.hostingEnvironment.helpUrl,
       assets: assetsUrl,
+      assetsVersion: config.assets.version
     },
     config.hostingEnvironment.env === 'dev',
   );
@@ -152,8 +153,7 @@ const init = async () => {
 
     server.listen(config.hostingEnvironment.port, () => {
       logger.info(
-        `Dev server listening on https://${config.hostingEnvironment.host}:${
-          config.hostingEnvironment.port
+        `Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port
         } with config:\n${JSON.stringify(config)}`,
       );
     });
