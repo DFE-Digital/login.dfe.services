052b09bba9e9cc664347703f6298264a2f3a936b
sumanta <sumanta.sr@gmail.com>
2020-10-26 13:53:10 +0000

    added account

--- src/app/users/resendInvitation.js
+++ src/app/users/resendInvitation.js
@@ -46,7 +46,7 @@ const validate = async (req) => {
   } else if (!emailPolicy.doesEmailMeetPolicy(model.email)) {
     model.validationMessages.email = 'Please enter a valid email address';
   } else {
-    const existingUser = await Account.getById(model.email, req.id);
+    const existingUser = await Account.getByEmail(model.email);
     const existingInvitation = await Account.getInvitationByEmail(model.email);
 
     if (existingUser || existingInvitation) {
