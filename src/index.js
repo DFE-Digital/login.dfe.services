cfcc1b2df5e712dd5a3883f3a9e2d51c248aa05b
Vijay <vijay.hirudayasamy@education.gov.uk>
2021-06-22 10:36:22 +0100

    session timeout has been fixed by adding session now value

--- src/index.js
+++ src/index.js
@@ -33,8 +33,6 @@ const init = async () => {
     expiryInMinutes = sessionExpiry;
   }
 
-  const expiryDate = new Date(Date.now() + 60 * expiryInMinutes * 1000);
-
   const app = express();
   app.use(
     helmet({
@@ -79,13 +77,18 @@ const init = async () => {
       resave: true,
       saveUninitialized: true,
       secret: config.hostingEnvironment.sessionSecret,
-      cookie: {
-        httpOnly: true,
-        secure: true,
-        expires: expiryDate,
-      },
+      maxAge: expiryInMinutes * 60000, // Expiry in milliseconds
+      httpOnly: true,
+      secure: true
     }),
   );
+
+  app.use((req, res, next) => {
+    req.session.now = Date.now();
+    next();
+  });
+
+
   app.use(flash());
 
   let assetsUrl = config.assets.url;
