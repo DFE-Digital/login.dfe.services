e264bd9b0204651c7af25ee722647d1d73a76bd1
Shivanshu Patel <shivanshu.patel@essensys.tech>
2021-06-23 13:36:11 +0100

    Implement HTTP Strict Transport Security

--- src/index.js
+++ src/index.js
@@ -34,14 +34,30 @@ const init = async () => {
   }
 
   const app = express();
-  app.use(
-    helmet({
-      noCache: true,
-      frameguard: {
-        action: 'deny',
-      },
-    }),
-  );
+  
+  if (config.hostingEnvironment.hstsMaxAge) {
+    app.use(
+      helmet({
+        noCache: true,
+        frameguard: {
+          action: 'deny',
+        },
+        hsts: {
+          maxAge: config.hostingEnvironment.hstsMaxAge,
+          preload: true,
+        }
+      })
+    );
+  } else {
+    app.use(
+      helmet({
+        noCache: true,
+        frameguard: {
+          action: 'deny',
+        },
+      }),
+    );
+  }
 
   if (config.hostingEnvironment.env !== 'dev') {
     app.set('trust proxy', 1);
