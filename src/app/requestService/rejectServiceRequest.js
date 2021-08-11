a565e41f79f84245e54d1b7cd5c0eddf67f4adbd
mrawat-dev <mrawat-dev@gmail.com>
2021-08-11 13:53:33 +0100

    Added reject a service journey

--- src/app/requestService/rejectServiceRequest.js
+++ src/app/requestService/rejectServiceRequest.js
@@ -1,19 +1,139 @@
-const { getAndMapOrgRequest } = require('../users/utils');
+'use strict';
+const { listRolesOfService, addUserService, getServicesForUser } = require('../../infrastructure/access');
+const { getUserDetails, getAndMapOrgRequest } = require('../users/utils');
 const { updateRequestById } = require('../../infrastructure/organisations');
+
 const logger = require('../../infrastructure/logger');
 const config = require('../../infrastructure/config');
-const NotificationClient = require('login.dfe.notifications.client');
 
+const PolicyEngine = require('login.dfe.policy-engine');
+const policyEngine = new PolicyEngine(config);
+
+const NotificationClient = require('login.dfe.notifications.client');
 const notificationClient = new NotificationClient({
   connectionString: config.notifications.connectionString,
 });
 
+const { checkCacheForAllServices } = require('../../infrastructure/helpers/allServicesAppCache');
+
+const getViewModel = async (req) => {
+  const organisationDetails = req.userOrganisations.find((x) => x.organisation.id === req.params.orgId);
+  const serviceId = req.params.sid;
+  const roleIds = JSON.parse(decodeURIComponent(req.params.rids))
+  const roles = roleIds || [];
+
+  const endUser = await getUserDetails(req);
+
+  const allServices = await checkCacheForAllServices(req.id);
+  const serviceDetails = allServices.services.find((x) => x.id === serviceId);
+  const allRolesOfService = await listRolesOfService(serviceId, req.id);
+  const roleDetails = allRolesOfService.filter((x) => roles.find((y) => y.toLowerCase() === x.id.toLowerCase()));
+
+  const service = {
+    serviceId,
+    name: serviceDetails.name,
+    roles: roleDetails,
+  };
+
+  const model = {
+    csrfToken: req.csrfToken(),
+    currentPage: 'users',
+    user: {
+      firstName: req.session.user.firstName,
+      lastName: req.session.user.lastName,
+      email: req.session.user.email,
+      uid: req.session.user.uid ? req.session.user.uid : '',
+    },
+    validationMessages: {},
+    endUserName: `${endUser.firstName} ${endUser.lastName}`,
+    endUserEmail: endUser.email,
+    service,
+    organisationDetails
+  }
+
+  return model
+};
+
 const get = async (req, res) => {
-   //WIP
+  if (!req.session.user) {
+    return res.redirect('/my-services');
+  }
+  
+  const model = await getViewModel(req)  
+  const roleIds = JSON.parse(decodeURIComponent(req.params.rids))
+  const roles = roleIds || [];
+
+  const policyValidationResult = await policyEngine.validate(
+    req.params.uid,
+    req.params.orgId,
+    req.params.sid,
+    roles,
+    req.id,
+  );
+
+  if (policyValidationResult.length > 0) {
+    model.validationMessages.roles = policyValidationResult.map((x) => x.message)  
+  }
+
+  if (!req.session.user) {
+    req.session.user = {};
+  }
+
+  const endUser = await getUserDetails(req);
+
+  req.session.user.uid = endUser.id
+  req.session.user.firstName = endUser.firstName
+  req.session.user.lastName = endUser.lastName
+  req.session.user.email = endUser.email
+  req.session.user.services = [model.service]
+
+  return res.render('requestService/views/rejectServiceRequest', model)
 };
 
 const post = async (req, res) => {
-   //WIP
+  if (!req.session.user) {
+    return res.redirect('/my-services');
+  }
+  let rejectReason = req.body.reason ? req.body.reason : "";
+
+  const model = await getViewModel(req)
+  const roleIds = JSON.parse(decodeURIComponent(req.params.rids))
+  const roles = roleIds || [];
+  
+  const policyValidationResult = await policyEngine.validate(
+    req.params.uid,
+    req.params.orgId,
+    req.params.sid,
+    roles,
+    req.id,
+  )
+
+  if (policyValidationResult.length > 0) {
+    model.validationMessages.roles = policyValidationResult.map((x) => x.message)
+    return res.render('requestService/views/rejectServiceRequest', model)
+  }
+
+  await notificationClient.sendServiceRequestRejected(
+    req.session.user.email, 
+    req.session.user.firstName, 
+    req.session.user.lastName, 
+    model.organisationDetails.organisation.name,
+    model.service.name,
+    model.service.roles.map(i => i.name),
+    rejectReason
+  )
+
+  logger.audit({
+    type: 'services',
+    subType: 'access-request-rejected',
+    userId: req.user.uid,
+    userEmail: req.user.email,
+    application: config.loggerSettings.applicationName,
+    env: config.hostingEnvironment.env,
+    message: `${req.user.email} (approverId: ${req.user.sub}) rejected service (serviceId: ${req.params.sid}), roles (roleIds: ${JSON.stringify(roles)}) and organisation (orgId: ${req.params.orgId}) for end user (endUserId: ${req.params.uid}). ${rejectReason ? `The reject reason is ${rejectReason}` : ''}`
+  });
+  
+  res.redirect(`/my-services`);
 };
 
 module.exports = {
