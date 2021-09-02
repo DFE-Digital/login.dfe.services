af5589e1a8821ecac299d721ffe52ba6b9a50d86
mrawat-dev <mrawat-dev@gmail.com>
2021-09-02 14:40:09 +0100

    Showing only approver orgs for manage users

--- src/app/users/usersList.js
+++ src/app/users/usersList.js
@@ -2,6 +2,7 @@
 const { mapUserStatus } = require('./../../infrastructure/utils');
 const { getAllUsersForOrg } = require('../../infrastructure/search');
 const { getById } = require('../../infrastructure/account');
+const { getApproverOrgsFromReq } = require('./utils');
 
 const clearUserSessionData = (req) => {
   if (req.session.user) {
@@ -10,35 +11,42 @@ const clearUserSessionData = (req) => {
 };
 
 const search = async (req) => {
-  const organisationId = req.params.orgId;
-  const organisationDetails = req.userOrganisations.find((x) => x.organisation.id === organisationId);
+  const approverOrgs = getApproverOrgsFromReq(req);
   const paramsSource = req.method === 'POST' ? req.body : req.query;
   let page = paramsSource.page ? parseInt(paramsSource.page) : 1;
   if (isNaN(page)) {
     page = 1;
   }
-
   let sortBy = paramsSource.sort ? paramsSource.sort : 'searchableName';
   let sortAsc = (paramsSource.sortdir ? paramsSource.sortdir : 'asc').toLowerCase() === 'asc';
 
-  const usersForOrganisation = await getAllUsersForOrg(page, organisationId, sortBy, sortAsc ? 'asc' : 'desc', req.id);
+  let orgIds = []
+  for(let i= 0; i < approverOrgs.length; i++) {
+    const org = approverOrgs[i]
+    const orgId = org.organisation.id
+    orgIds.push(orgId)
+  }
+
+  const usersForOrganisation = await getAllUsersForOrg(page, orgIds, sortBy, sortAsc ? 'asc' : 'desc', req.id);
+  
   for (let i = 0; i < usersForOrganisation.users.length; i++) {
     const user = usersForOrganisation.users[i];
     if (req.user.sub === user.id) {
       const me = await getById(req.user.sub);
       user.email = me.claims.email;
     }
-    // TODO review logic to get all orgs
-    // const organisation = user.organisations.filter((x) => x.id === organisationId);
+    
+    const organisation = user.organisations.filter((x) => orgIds.includes(x.id));
     user.statusId = mapUserStatus(user.statusId);
-    user.organisations = user.organisations;
+    user.organisations = organisation;
   }
+
   return {
     page,
     sortBy,
     sortOrder: sortAsc ? 'asc' : 'desc',
     usersForOrganisation,
-    organisationDetails,
+    orgIds,
     numberOfPages: usersForOrganisation.numberOfPages,
     totalNumberOfResults: usersForOrganisation.totalNumberOfResults,
     sort: {
@@ -62,9 +70,29 @@ const search = async (req) => {
   };
 };
 
+const buildInviteUserLink = (orgIds) => {
+  if(orgIds && orgIds.length === 1) {
+    return `/approvals/${orgIds[0]}/users/new-user`
+  }
+  //TODO: https://dfe-secureaccess.atlassian.net/browse/NSA-5108
+  return ""
+}
+
+const buildRequestsLink = (orgIds) => {
+  if(orgIds && orgIds.length === 1) {
+    return `/access-requests/${orgIds[0]}/requests`
+  }
+  //TODO: https://dfe-secureaccess.atlassian.net/browse/NSA-5109
+  return ""
+}
+
 const get = async (req, res) => {
   clearUserSessionData(req);
+
   const result = await search(req);
+  const inviteUserUrl = buildInviteUserLink(result.orgIds)
+  const requestsUrl = buildRequestsLink(result.orgIds)
+
   return res.render('users/views/usersList', {
     title: 'Manage users',
     csrfToken: req.csrfToken(),
@@ -76,8 +104,8 @@ const get = async (req, res) => {
     sortOrder: result.sortOrder,
     numberOfPages: result.numberOfPages,
     totalNumberOfResults: result.totalNumberOfResults,
-    organisationDetails: result.organisationDetails,
-    organisationStatus: result.organisationDetails.organisation.status.id,
+    inviteUserUrl,
+    requestsUrl
   });
 };
 
@@ -93,8 +121,7 @@ const post = async (req, res) => {
     sortBy: result.sortBy,
     sortOrder: result.sortOrder,
     numberOfPages: result.numberOfPages,
-    totalNumberOfResults: result.totalNumberOfResults,
-    organisationDetails: result.organisationDetails,
+    totalNumberOfResults: result.totalNumberOfResults
   });
 };
 
