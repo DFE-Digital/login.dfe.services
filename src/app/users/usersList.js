9e8b66e1e67fecd5f0ea7511ff1b486d654cf27b
mrawat-dev <mrawat-dev@gmail.com>
2021-09-02 15:01:56 +0100

    Renamed variables

--- src/app/users/usersList.js
+++ src/app/users/usersList.js
@@ -20,14 +20,14 @@ const search = async (req) => {
   let sortBy = paramsSource.sort ? paramsSource.sort : 'searchableName';
   let sortAsc = (paramsSource.sortdir ? paramsSource.sortdir : 'asc').toLowerCase() === 'asc';
 
-  let orgIds = []
+  let approverOrgIds = []
   for(let i= 0; i < approverOrgs.length; i++) {
     const org = approverOrgs[i]
     const orgId = org.organisation.id
-    orgIds.push(orgId)
+    approverOrgIds.push(orgId)
   }
 
-  const usersForOrganisation = await getAllUsersForOrg(page, orgIds, sortBy, sortAsc ? 'asc' : 'desc', req.id);
+  const usersForOrganisation = await getAllUsersForOrg(page, approverOrgIds, sortBy, sortAsc ? 'asc' : 'desc', req.id);
   
   for (let i = 0; i < usersForOrganisation.users.length; i++) {
     const user = usersForOrganisation.users[i];
@@ -36,7 +36,7 @@ const search = async (req) => {
       user.email = me.claims.email;
     }
     
-    const approverUserOrgs = user.organisations.filter((x) => orgIds.includes(x.id));
+    const approverUserOrgs = user.organisations.filter((x) => approverOrgIds.includes(x.id));
     user.statusId = mapUserStatus(user.statusId);
     user.organisations = approverUserOrgs;
   }
@@ -46,7 +46,7 @@ const search = async (req) => {
     sortBy,
     sortOrder: sortAsc ? 'asc' : 'desc',
     usersForOrganisation,
-    orgIds,
+    approverOrgIds,
     numberOfPages: usersForOrganisation.numberOfPages,
     totalNumberOfResults: usersForOrganisation.totalNumberOfResults,
     sort: {
@@ -90,8 +90,8 @@ const get = async (req, res) => {
   clearUserSessionData(req);
 
   const result = await search(req);
-  const inviteUserUrl = buildInviteUserLink(result.orgIds)
-  const requestsUrl = buildRequestsLink(result.orgIds)
+  const inviteUserUrl = buildInviteUserLink(result.approverOrgIds)
+  const requestsUrl = buildRequestsLink(result.approverOrgIds)
 
   return res.render('users/views/usersList', {
     title: 'Manage users',
