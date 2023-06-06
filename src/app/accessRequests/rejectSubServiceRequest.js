2218436a7ed09144134957a5a2e49e5ea90f7d91
stottidfe <peter.stott@education.gov.uk>
2023-06-06 12:23:22 +0100

    fixed reject error

--- src/app/accessRequests/rejectSubServiceRequest.js
+++ src/app/accessRequests/rejectSubServiceRequest.js
@@ -1,6 +1,7 @@
 const {updateServiceRequest} = require('../requestService/utils');
+const Account = require('./../../infrastructure/account');
 const { isServiceEmailNotificationAllowed } = require('../../../src/infrastructure/applications');
-const { getSubServiceRequestVieModel, getAndMapServiceRequest } = require('./utils');
+const { getSubServiceRequestVieModel, getAndMapServiceRequest, getRoleAndServiceNames, generateFlashMessages } = require('./utils');
 const logger = require('./../../infrastructure/logger');
 const config = require('./../../infrastructure/config');
 const NotificationClient = require('login.dfe.notifications.client');
@@ -15,12 +16,12 @@ const validate = async (req) => {
     if( req.session.roleIds !== viewModel.role_ids)
     {
     viewModel.role_ids = req.session.roleIds;
-     let submodel =  await getRoleAndServiceNames(viewModel, req.param.rid, req);
+     let submodel =  await getRoleAndServiceNames(viewModel, req.params.rid, req);
      viewModel.roles = submodel.roles.filter(x => x !== undefined);
-     req.session.roleIds = undefined;
+     //req.session.roleIds = undefined;
      req.session.roles = viewModel.roles;
     }else{
-      req.session.roleIds = undefined;
+     // req.session.roleIds = undefined;
       req.session.roles = viewModel.roles;
     }
 }
@@ -44,12 +45,24 @@ const get = async (req, res) => {
     const model = await getAndMapServiceRequest(req.params.rid); 
     const viewModel = await getSubServiceRequestVieModel(model, req.id, req);
     req.session.rid = req.params.rid;
+    if (viewModel.actioned_by && (viewModel.status === -1 || 1 )) {
+      const user = await Account.getById(viewModel.actioned_by);
+      const { title, heading, message } = generateFlashMessages(
+        'service',
+        viewModel.status,user.claims.email,viewModel.endUsersGivenName,viewModel.endUsersFamilyName, viewModel.roles.map((i) => i.name),
+        res,
+      );
+      res.flash('title', `${title}`);
+      res.flash('heading', `${heading}`);
+      res.flash('message', `${message}`);
+      return res.redirect(`/access-requests/requests`);
+    }
     if( req.session.roleIds !== undefined)
   {
     if( req.session.roleIds !== viewModel.role_ids)
     {
     viewModel.role_ids = req.session.roleIds;
-     let submodel =  await getRoleAndServiceNames(viewModel, req.param.rid, req);
+     let submodel =  await getRoleAndServiceNames(viewModel, req.params.rid, req);
      viewModel.roles = submodel.roles.filter(x => x !== undefined);
      //req.session.roleIds = undefined;
      req.session.roles = viewModel.roles;
@@ -77,51 +90,71 @@ const post = async (req, res) => {
     model.viewModel.csrfToken = req.csrfToken();
     return res.render('accessRequests/views/rejectSubServiceRequest', model.viewModel);
   }
-  //reqId, statusId, approverId, reason
-  const request = await updateServiceRequest(req.params.rid,-1,req.user.sub,model.reason);
- if(request.success)
- {
-  //send rejected email
-  const isEmailAllowed = await isServiceEmailNotificationAllowed();
-  if (isEmailAllowed) {
-    const notificationClient = new NotificationClient({
-      connectionString: config.notifications.connectionString,
-    });
-    await notificationClient.sendSubServiceRequestRejected(
-      model.viewModel.endUsersEmail,
-          model.viewModel.endUsersGivenName,
-          model.viewModel.endUsersFamilyName,
-          model.viewModel.org_name,
-          model.viewModel.Service_name,
-          model.viewModel.roles.map((i) => i.name),
-          model.reason,
+  if (model.viewModel.actioned_by && (model.viewModel.status === -1 || 1 )) {
+    const user = await Account.getById(model.viewModel.actioned_by);
+    const { title, heading, message } = generateFlashMessages(
+      'service',
+      model.viewModel.status,user.claims.email,model.viewModel.endUsersGivenName,model.viewModel.endUsersFamilyName, model.viewModel.roles.map((i) => i.name),
+      res,
     );
+    res.flash('title', `${title}`);
+    res.flash('heading', `${heading}`);
+    res.flash('message', `${message}`);
+    return res.redirect(`/access-requests/requests`);
   }
-  logger.audit({
-    type: 'sub-service',
-    subType: 'sub-service request Rejected',
-    userId: req.user.sub,
-    userEmail: req.user.email,
-    application: config.loggerSettings.applicationName,
-    env: config.hostingEnvironment.env,
-    message: `${req.user.email} (approverId: ${
-      req.user.sub
-    }) rejected sub-service request for (serviceId: ${model.viewModel.service_id}) and sub-services (roleIds: ${JSON.stringify(
-      model.viewModel.role_ids,
-    )}) for organisation (orgId: ${model.viewModel.org_id}) for end user (endUserId: ${model.viewModel.user_id}). ${
-      model.reason ? `The reject reason is ${model.reason}` : ''
-    } - requestId (reqId: ${req.params.rid})`,
-  });
+  else
+  {
+      //reqId, statusId, approverId, reason
+    const request = await updateServiceRequest(req.params.rid,-1,req.user.sub ,model.reason);
+    if(request.success)
+    {
+      //send rejected email
+      const isEmailAllowed = await isServiceEmailNotificationAllowed();
+      if (isEmailAllowed) {
+        const notificationClient = new NotificationClient({
+          connectionString: config.notifications.connectionString,
+        });
+        await notificationClient.sendSubServiceRequestRejected(
+          model.viewModel.endUsersEmail,
+              model.viewModel.endUsersGivenName,
+              model.viewModel.endUsersFamilyName,
+              model.viewModel.org_name,
+              model.viewModel.Service_name,
+              model.viewModel.roles.map((i) => i.name),
+              model.reason,
+        );
+      }
+      logger.audit({
+        type: 'sub-service',
+        subType: 'sub-service request Rejected',
+        userId: req.user.sub,
+        userEmail: req.user.email,
+        application: config.loggerSettings.applicationName,
+        env: config.hostingEnvironment.env,
+        message: `${req.user.email} (approverId: ${
+          req.user.sub
+        }) rejected sub-service request for (serviceId: ${model.viewModel.service_id}) and sub-services (roleIds: ${JSON.stringify(
+          model.viewModel.role_ids,
+        )}) for organisation (orgId: ${model.viewModel.org_id}) for end user (endUserId: ${model.viewModel.user_id}). ${
+          model.reason ? `The reject reason is ${model.reason}` : ''
+        } - requestId (reqId: ${req.params.rid})`,
+      });
 
-  res.flash('title', 'Success');
-  res.flash('heading', 'Sub-service request rejected');
-  res.flash(
-    'message',
-    'The user who raised the request will receive an email to tell them their sub-service access request has been rejected.',
-  );
+      res.flash('title', 'Success');
+      res.flash('heading', 'Sub-service request rejected');
+      res.flash(
+        'message',
+        'The user who raised the request will receive an email to tell them their sub-service access request has been rejected.',
+      );
 
-  return res.redirect(`/access-requests/requests`);
- }
+      return res.redirect(`/access-requests/requests`);
+    }
+    else{
+      model.viewModel.csrfToken = req.csrfToken();
+      model.viewModel.validationMessages.reason = 'an error occured during save please contact support';
+      return res.render('accessRequests/views/rejectSubServiceRequest', model.viewModel);
+    }
+  }
 };
 module.exports = {
   get,
