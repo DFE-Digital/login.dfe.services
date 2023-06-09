919f960d9da9aab1f2f58e6ed6e3fab63afdc2a1
stottidfe <peter.stott@education.gov.uk>
2023-06-09 14:29:42 +0100

    unt test complete

--- test/unit/app/accessRequests/rejectSubServiceRequest.post.test.js
+++ test/unit/app/accessRequests/rejectSubServiceRequest.post.test.js
@@ -1,14 +1,18 @@
 const { mockRequest, mockResponse, mockAdapterConfig } = require('../../../utils/jestMocks');
-const { getSubServiceRequestVieModel, getAndMapServiceRequest, getNewRoleDetails } = require('../../../../src/app/accessRequests/utils');
-const {updateServiceRequest} = require('../../../../src/app/requestService/utils');
-const {post } = require('../../../../src/app/accessRequests/reviewSubServiceRequest');
+const {
+  getSubServiceRequestVieModel,
+  getAndMapServiceRequest,
+  getNewRoleDetails,
+} = require('../../../../src/app/accessRequests/utils');
+const { updateServiceRequest } = require('../../../../src/app/requestService/utils');
+const { post } = require('../../../../src/app/accessRequests/reviewSubServiceRequest');
 const NotificationClient = require('login.dfe.notifications.client');
 const sendAccessRequest = jest.fn();
 
 const Account = require('../../../../src/infrastructure/account');
 jest.mock('login.dfe.policy-engine');
 jest.mock('../../../../src/infrastructure/config', () => {
-  return mockAdapterConfig()
+  return mockAdapterConfig();
 });
 jest.mock('../../../../src/infrastructure/logger', () => require('./../../../utils/jestMocks').mockLogger());
 jest.mock('./../../../../src/infrastructure/account', () => ({
@@ -25,10 +29,10 @@ jest.mock('../../../../src/app/accessRequests/utils', () => {
 });
 
 jest.mock('../../../../src/app/requestService/utils', () => {
-    return {
-        updateServiceRequest: jest.fn(),
-    };
-  });
+  return {
+    updateServiceRequest: jest.fn(),
+  };
+});
 
 jest.mock('../../../../src/infrastructure/config', () => {
   return mockAdapterConfig();
@@ -41,36 +45,52 @@ jest.mock('login.dfe.dao', () => {
     },
   };
 });
-const listRoles = [{
-  code:'ASP_School_Anon', id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
-  name: 'ASP School Anon',
-  numericId: '124'},{
-    code:'ASP_School_Anon', id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
+const listRoles = [
+  {
+    code: 'ASP_School_Anon',
+    id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
     name: 'ASP School Anon',
-    numericId: '124'},{
-      code:'ASP_School_Anon', id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
-      name: 'ASP School Anon',
-      numericId: '124'},{
-        code:'ASP_School_Anon', id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
-        name: 'ASP School Anon',
-        numericId: '124'},{
-          code:'ASP_School_Anon', id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
-          name: 'ASP School Anon',
-          numericId: '124'}];
+    numericId: '124',
+  },
+  {
+    code: 'ASP_School_Anon',
+    id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
+    name: 'ASP School Anon',
+    numericId: '124',
+  },
+  {
+    code: 'ASP_School_Anon',
+    id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
+    name: 'ASP School Anon',
+    numericId: '124',
+  },
+  {
+    code: 'ASP_School_Anon',
+    id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
+    name: 'ASP School Anon',
+    numericId: '124',
+  },
+  {
+    code: 'ASP_School_Anon',
+    id: '01379D9F-A6DF-4810-A6C4-5468CBD41E42',
+    name: 'ASP School Anon',
+    numericId: '124',
+  },
+];
 const viewModel = {
-  endUsersEmail : 'b@b.gov.uk',
-  endUsersFamilyName :'b',
-  endUsersGivenName : 'b',
-  org_name :'org1',
-  org_id : 'org1',
-  user_id : 'endUser1',
-  role_ids : 'role1',
-  service_id :'service1',
-  status : 0,
-  actioned_reason : 'Pending',
-  actioned_by : null,
-  reason : 'Pending',
-  csrfToken : null,
+  endUsersEmail: 'b@b.gov.uk',
+  endUsersFamilyName: 'b',
+  endUsersGivenName: 'b',
+  org_name: 'org1',
+  org_id: 'org1',
+  user_id: 'endUser1',
+  role_ids: 'role1',
+  service_id: 'service1',
+  status: 0,
+  actioned_reason: 'Pending',
+  actioned_by: null,
+  reason: 'Pending',
+  csrfToken: null,
   selectedResponse: null,
   validationMessages: {},
   currentPage: 'requests',
@@ -82,31 +102,40 @@ const model = {
   _options: null,
   _previousDataValues: null,
   approverEmail: '',
-  approverName:'',
-  dataValues : {id: 'request1', actioned_by: null, actioned_at: null,actioned_reason:'Pending', createdAt: new Date(),organisation_id:'org1', reason:'',role_ids: 'role1', service_id: 'service1', status: 0, updatedAt: new Date(),user_id:'endUser1'},
-  endUsersEmail: 'b@b.gov.uk', 
-  endUsersFamilyName: 'b', 
-  endUsersGvenName:'b',
+  approverName: '',
+  dataValues: {
+    id: 'request1',
+    actioned_by: null,
+    actioned_at: null,
+    actioned_reason: 'Pending',
+    createdAt: new Date(),
+    organisation_id: 'org1',
+    reason: '',
+    role_ids: 'role1',
+    service_id: 'service1',
+    status: 0,
+    updatedAt: new Date(),
+    user_id: 'endUser1',
+  },
+  endUsersEmail: 'b@b.gov.uk',
+  endUsersFamilyName: 'b',
+  endUsersGvenName: 'b',
   isNewRecord: false,
-  organisation:{id: 'org1', name: 'accademic organisatioon'}
+  organisation: { id: 'org1', name: 'accademic organisatioon' },
 };
 jest.mock('../../../../src/app/users/utils');
-
 
 describe('When reviewing a sub-service request for rejecting', () => {
   let req;
   let res;
 
-
-
   beforeEach(() => {
     req = mockRequest({
-        params : {
+      params: {
         rid: 'sub-service-req-ID',
       },
-      session:{
-        user:{ sub: 'user1',
-        email: 'email@email.com',},
+      session: {
+        user: { sub: 'user1', email: 'email@email.com' },
       },
       user: {
         sub: 'user1',
@@ -115,70 +144,79 @@ describe('When reviewing a sub-service request for rejecting', () => {
       body: {
         selectedResponse: 'reject',
       },
-      model : {
+      model: {
         _changed: 0,
         _options: null,
         _previousDataValues: null,
         approverEmail: '',
-        approverName:'',
-        dataValues : {id: 'request1', actioned_by: null, actioned_at: null,actioned_reason:'Pending', createdAt: new Date(),organisation_id:'org1', reason:'reject',role_ids: 'role1', service_id: 'service1', status: 0, updatedAt: new Date(),user_id:'endUser1'},
-        endUsersEmail: 'b@b.gov.uk', 
-        endUsersFamilyName: 'b', 
-        endUsersGvenName:'b',
+        approverName: '',
+        dataValues: {
+          id: 'request1',
+          actioned_by: null,
+          actioned_at: null,
+          actioned_reason: 'Pending',
+          createdAt: new Date(),
+          organisation_id: 'org1',
+          reason: 'reject',
+          role_ids: 'role1',
+          service_id: 'service1',
+          status: 0,
+          updatedAt: new Date(),
+          user_id: 'endUser1',
+        },
+        endUsersEmail: 'b@b.gov.uk',
+        endUsersFamilyName: 'b',
+        endUsersGvenName: 'b',
         isNewRecord: false,
-        organisation:{id: 'org1', name: 'accademic organisatioon'}
+        organisation: { id: 'org1', name: 'accademic organisatioon' },
       },
-      viewModel : 
-        {
-          endUsersEmail : 'b@b.gov.uk',
-          endUsersFamilyName :'b',
-          endUsersGivenName : 'b',
-          org_name :'org1',
-          org_id : 'org1',
-          user_id : 'endUser1',
-          role_ids : 'role1',
-          service_id :'service1',
-          status : 0,
-          actioned_reason : 'Pending',
-          actioned_by : null,
-          reason : 'Pending',
-          csrfToken : null,
-          selectedResponse: 'reject',
-          validationMessages: {},
-          currentPage: 'requests',
-          Role_name: 'role  one',
-          service_name: 'service one',
-        }});
-      
-   
+      viewModel: {
+        endUsersEmail: 'b@b.gov.uk',
+        endUsersFamilyName: 'b',
+        endUsersGivenName: 'b',
+        org_name: 'org1',
+        org_id: 'org1',
+        user_id: 'endUser1',
+        role_ids: 'role1',
+        service_id: 'service1',
+        status: 0,
+        actioned_reason: 'Pending',
+        actioned_by: null,
+        reason: 'Pending',
+        csrfToken: null,
+        selectedResponse: 'reject',
+        validationMessages: {},
+        currentPage: 'requests',
+        Role_name: 'role  one',
+        service_name: 'service one',
+      },
+    });
+
     res = mockResponse();
     sendAccessRequest.mockReset();
-    
+
     Account.getById
-    .mockReset()
-    .mockReturnValue([
-      { claims: { sub: 'user-id-1', given_name: 'User', family_name: 'One', email: 'user.one@unit.tests' } },
-      { claims: { sub: 'user-id-2', given_name: 'User', family_name: 'Two', email: 'user.two@unit.tests' } },
-    ]);
+      .mockReset()
+      .mockReturnValue([
+        { claims: { sub: 'user-id-1', given_name: 'User', family_name: 'One', email: 'user.one@unit.tests' } },
+        { claims: { sub: 'user-id-2', given_name: 'User', family_name: 'Two', email: 'user.two@unit.tests' } },
+      ]);
 
     updateServiceRequest.mockReset();
-    updateServiceRequest.mockReturnValue(request = {success : true});
+    updateServiceRequest.mockReturnValue((request = { success: true }));
 
     getAndMapServiceRequest.mockReset();
     getAndMapServiceRequest.mockReturnValue(model);
 
     getSubServiceRequestVieModel.mockReset();
     getSubServiceRequestVieModel.mockReturnValue(viewModel);
-     getNewRoleDetails.mockReset();
-     getNewRoleDetails.mockReturnValue(listRoles);
-
+    getNewRoleDetails.mockReset();
+    getNewRoleDetails.mockReturnValue(listRoles);
   });
 
   it('then it should redirect to reject sub service view', async () => {
     await post(req, res);
     expect(res.redirect.mock.calls).toHaveLength(1);
     expect(res.redirect.mock.calls[0][0]).toBe(`/access-requests/subService-requests/${req.params.rid}/rejected`);
-    
   });
-
 });
