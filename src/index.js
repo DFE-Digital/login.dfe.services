e1b6e3437a489749703d15bcc5bd573fecd53b02
malikimran34 <malikimran21@icloud.com>
2023-10-19 21:54:45 +0100

    Update helmet snippet to allow domains and urls
    New version of helmet requires sources to be stated explicitly for image, font, scripts etc

--- src/index.js
+++ src/index.js
@@ -36,28 +36,46 @@ const init = async () => {
   const app = express();
 
   if (config.hostingEnvironment.hstsMaxAge) {
-    app.use(
-      helmet({
-        noCache: true,
-        frameguard: {
-          action: 'deny',
-        },
-        hsts: {
-          maxAge: config.hostingEnvironment.hstsMaxAge,
-          preload: true,
-        },
-      }),
-    );
-  } else {
-    app.use(
-      helmet({
-        noCache: true,
-        frameguard: {
-          action: 'deny',
-        },
-      }),
-    );
+    app.use(helmet({
+      noCache: true,
+      frameguard: {
+        action: 'deny',
+      },
+      hsts: {
+        maxAge: config.hostingEnvironment.hstsMaxAge,
+        preload: true,
+      },
+    }));
   }
+
+  logger.info('set helmet policy defaults');
+
+  // Setting helmet Content Security Policy
+  const scriptSources = ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'localhost', '*.signin.education.gov.uk'];
+
+  app.use(helmet.contentSecurityPolicy({
+    browserSniff: false,
+    setAllHeaders: false,
+    directives: {
+      defaultSrc: ["'self'"],
+      childSrc: ["'none'"],
+      objectSrc: ["'none'"],
+      scriptSrc: scriptSources,
+      styleSrc: ["'self'", "'unsafe-inline'", 'localhost', '*.signin.education.gov.uk'],
+      imgSrc: ["'self'", 'data:', 'blob:', 'localhost', '*.signin.education.gov.uk'],
+      fontSrc: ["'self'", 'data:', '*.signin.education.gov.uk'],
+      connectSrc: ["'self'"],
+      formAction: ["'self'", '*'],
+    },
+  }));
+
+  logger.info('Set helmet filters');
+
+  app.use(helmet.xssFilter());
+  app.use(helmet.frameguard('false'));
+  app.use(helmet.ieNoOpen());
+
+  logger.info('helmet setup complete');
 
   if (config.hostingEnvironment.env !== 'dev') {
     app.set('trust proxy', 1);
