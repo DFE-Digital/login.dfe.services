03871c046e010dd3b60c289169eaf4544b179721
Adam Mann <manny160389@hotmail.com>
2024-08-23 10:52:35 +0100

    [NSA-8432] Fix skipped unit tests (#587)
    * Commit test fixing progress
    
    * Tidy and remove remaining skipped tests

--- test/unit/infrastructure/account/ApiAccountTests/getUsersById.spec.js
+++ test/unit/infrastructure/account/ApiAccountTests/getUsersById.spec.js
@@ -1,13 +1,8 @@
 const { mockAdapterConfig } = require('../../../../utils/jestMocks');
 
-jest.mock('login.dfe.async-retry', () => ({
-  fetchApi: jest.fn(),
-}));
-jest.mock('login.dfe.jwt-strategies');
 jest.mock('./../../../../../src/infrastructure/config', () => {
   return mockAdapterConfig();
 });
-const { fetchApi } = require('login.dfe.async-retry');
 jest.mock('login.dfe.dao', () => {
   return {
     directories: {
@@ -17,61 +12,41 @@ jest.mock('login.dfe.dao', () => {
             email: 'kevin.lewis@hq.local',
             sub: 'F47D8673-8861-4A95-8286-000403EED219',
           },
+          {
+            email: 'another.person@hq.local',
+            sub: 'A14B8673-7752-4A95-8286-000403CCD912',
+          },
         ];
       },
     },
   };
 });
 
-describe('When getting a collection of users', () => {
-  const user = { sub: 'user1', email: 'user.one@unit.test' };
+describe('When getting a collection of users - getUsersById', () => {
+  const userIds = ['F47D8673-8861-4A95-8286-000403EED219', 'A14B8673-7752-4A95-8286-000403CCD912'];
   const users = [
-    { sub: 'user1', email: 'user.one@unit.test' },
-    { sub: 'user2', email: 'user.two@unit.test' },
+    {
+      email: 'kevin.lewis@hq.local',
+      sub: 'F47D8673-8861-4A95-8286-000403EED219',
+    },
+    {
+      email: 'another.person@hq.local',
+      sub: 'A14B8673-7752-4A95-8286-000403CCD912',
+    },
   ];
-  const userIds = ['user1', 'user2'];
 
   let Account;
-  let getBearerToken;
 
   beforeEach(() => {
-    getBearerToken = jest.fn().mockReturnValue('token');
-    const jwtStrategy = require('login.dfe.jwt-strategies');
-    jwtStrategy.mockImplementation(() => ({
-      getBearerToken,
-    }));
-
-    fetchApi.mockReset().mockReturnValue([]);
 
     Account = require('./../../../../../src/infrastructure/account/DirectoriesApiAccount');
   });
 
-  it.only('then it should get users in the directories api', async () => {
+  it('should get correct users', async () => {
     let result = await Account.getUsersById(userIds);
-    expect(result).toBeDefined();
-    expect(result).toHaveLength(1);
-  });
-
-  it('then it should get correct user', async () => {
-    let result = await Account.getUsersById(userIds);
-    expect(result[0].sub).toBe('F47D8673-8861-4A95-8286-000403EED219');
-  });
-
-  it('then it should return a list of users', async () => {
-    fetchApi.mockImplementation(() => users);
-
-    const actual = await Account.getUsersById(userIds);
-
-    expect(actual).toEqual([new Account(users[0]), new Account(users[1])]);
-  });
-
-  it('then it should reject if password change fails', async () => {
-    fetchApi.mockImplementation(() => {
-      const error = new Error('Unit test');
-      error.statusCode = 401;
-      throw error;
-    });
-
-    await expect(Account.getUsersById(userIds)).rejects.toBeDefined();
+    expect(result).toHaveLength(2);
+    expect(result).toEqual([new Account(users[0]), new Account(users[1])]);
+    expect(result[0].claims.sub).toBe('F47D8673-8861-4A95-8286-000403EED219');
+    expect(result[1].claims.sub).toBe('A14B8673-7752-4A95-8286-000403CCD912');
   });
 });
