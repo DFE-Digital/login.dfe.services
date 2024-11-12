35162a9cf1a0226dc50f5de14f4beacddf566305
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2024-11-12 12:45:10 +0000

    Update index.js

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -1,29 +1,196 @@
-'use strict';
-
-const fs = require('fs');
-const Path = require('path');
-const config = require('../../../config/services.js');
-
-const getSettingsObject = (settings) => {
-  try {
-    return JSON.parse(settings);
-  } catch (e) {
-    return null;
-  }
-};
-
-const getSettingsFromFile = (settingsPath) => {
-  if (fs.existsSync(settingsPath)) {
-    const file = fs.readFileSync(settingsPath, 'utf8');
-    try {
-      return JSON.parse(file);
-    } catch (e) {
-      return null;
+const config = {
+  loggerSettings: {
+    logLevel: "debug",
+    applicationName: "Services",
+    auditDb: {
+      host: process.env.PLATFORM_GLOBAL_SERVERNAME,
+      username: process.env.SVC_SIGNIN_ADT,
+      password: process.env.SVC_SIGNIN_ADTPASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_AUDITDATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
+  },
+  hostingEnvironment: {
+    useDevViews: false,
+    env: "azure",
+    host: process.env.STANDALONE_SERVICES_HOST_NAME,
+    hstsMaxAge: 86400,
+    port: 443,
+    protocol: "https",
+    sessionSecret: process.env.SESSION_ENCRYPTION_SECRET_SVC,
+    gaTrackingId: process.env.GOOGLE_ANALYTICS_ID,
+    serviceId: process.env.SERVICE_ID,
+    profileUrl: "https://" + process.env.STANDALONE_PROFILE_HOST_NAME,
+    interactionsUrl: "https://" + process.env.STANDALONE_INTERACTIONS_HOST_NAME,
+    helpUrl: "https://" + process.env.STANDALONE_HELP_HOST_NAME,
+    helpAssistantUrl: process.env.HELP_ASSISTANT_URL,
+    supportUrl: "https://" + process.env.STANDALONE_SUPPORT_HOST_NAME,
+    surveyUrl: process.env.PLATFORM_GLOBAL_USER_FEEDBACK_URL,
+    applicationInsights: process.env.APP_INSIGHTS_INSTRUMENTATION_KEY,
+    sessionCookieExpiryInMinutes: 20,
+    agentKeepAlive: {
+      maxSockets: 30,
+      maxFreeSockets: 10,
+      timeout: 60000,
+      keepAliveTimeout: 30000
+    },
+    environmentBannerMessage: process.env.ENVIRONMENT_BANNER_MESSAGE
+  },
+  directories: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_DIRECTORIES_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANTDOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  organisations: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_ORGANISATIONS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANTDOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  access: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_ACCESS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  applications: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_APPLICATIONS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANTDOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  search: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_SEARCH_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  database: {
+    host: process.env.PLATFORM_GLOBAL_SERVERNAME,
+    username: process.env.SVC_SIGNIN_ORG,
+    password: process.env.SVC_SIGNIN_ORGPASSWORD,
+    dialect: "mssql",
+    name: process.env.PLATFORM_GLOBAL_ORGANISATIONSDATABASE_NAME,
+    encrypt: true,
+    schema: "dbo",
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  identifyingParty: {
+    url: "https://" + process.env.STANDALONE_OIDC_HOST_NAME,
+    clientId: process.env.IDENTIFYING_PARTY_ID,
+    clientSecret: process.env.IDENTIFYING_PARTY_SECRET,
+    clockTolerance: 300
+  },
+  toggles: {
+    generateUserOrgIdentifiers: true,
+    useApproverJourney: true,
+    environmentName: process.env.ENVIRONMENT_NAME
+  },
+  notifications: {
+    connectionString: process.env.REDIS_CONN + "/4?tls=true"
+  },
+  organisationRequests: {
+    requestLimit: 30
+  },
+  adapter: {
+    type: "sequelize",
+    directories: {
+      host: process.env.PLATFORM_GLOBAL_SERVERNAME,
+      username: process.env.SVC_SIGNIN_DIR,
+      password: process.env.SVC_SIGNIN_DIR_PASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_DIRECTORIES_DATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    },
+    organisation: {
+      host: process.env.PLATFORM_GLOBAL_SERVERNAME,
+      username: process.env.SVC_SIGNIN_ORG,
+      password: process.env.SVC_SIGNIN_ORG_PASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_ORGANISATIONS_DATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+  },
+  cookieSessionRedis: {
+    type: "redis",
+    params: {
+      connectionString: process.env.REDIS_CONN + "/3?tls=true"
+      }
+  },
+  assets: {
+    url: process.env.CDN_HOST_NAME,
+      version: process.env.CDN_ASSETS_VERSION
   }
-  return null;
-};
+}
 
-const fetchConfig = () => config
+process.env.settings = config;
 
-module.exports = fetchConfig();
+module.exports = config;
