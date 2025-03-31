051385c6c2d4222e76cf179b658577c686fff736
Adam Mann <Adam.Mann@education.gov.uk>
2025-03-31 11:54:19 +0100

    Remove logger for console

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -1,11 +1,8 @@
-
-
 const fs = require('fs');
 const Path = require('path');
-const logger = require("./infrastructure/logger");
 
 const getSettingsObject = (settings) => {
-  logger.info("getting settings from object");
+  console.log("getting settings from object");
   try {
     return JSON.parse(settings);
   } catch (e) {
@@ -14,7 +11,7 @@ const getSettingsObject = (settings) => {
 };
 
 const getSettingsFromFile = (settingsPath) => {
-  logger.info("in settings from file");
+  console.log("in settings from file");
   if (fs.existsSync(settingsPath)) {
     const file = fs.readFileSync(settingsPath, 'utf8');
     try {
@@ -30,16 +27,16 @@ const fetchConfig = () => {
   if (process.env.settings) {
     const settings = process.env.settings;
     let settingsObject = getSettingsObject(settings);
-    logger.info("After settings from object");
-    logger.info(settingsObject);
+    console.log("After settings from object");
+    console.log(settingsObject);
     if (settingsObject !== null) {
       return settingsObject;
     }
     const settingsPath = Path.resolve(settings);
     if (fs.existsSync(settingsPath)) {
       settingsObject = getSettingsFromFile(settingsPath);
-      logger.info("After settings from file");
-      logger.info(settingsObject);
+      console.log("After settings from file");
+      console.log(settingsObject);
       if (settingsObject !== null) {
         return settingsObject;
       }
