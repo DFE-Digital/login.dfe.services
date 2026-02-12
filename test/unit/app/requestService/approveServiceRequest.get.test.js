7c1e2186b33191bf10f6d94b04d5ed85f2ad9d14
Adam Mann <Adam.Mann@education.gov.uk>
2026-02-12 12:48:14 +0000

    Fix unit tests

--- test/unit/app/requestService/approveServiceRequest.get.test.js
+++ test/unit/app/requestService/approveServiceRequest.get.test.js
@@ -168,6 +168,9 @@ describe("When reviewing a service request for approving", () => {
         {
           id: "service1",
           name: "service name",
+          relyingParty: {
+            clientId: "serviceClient1",
+          },
         },
       ],
     });
